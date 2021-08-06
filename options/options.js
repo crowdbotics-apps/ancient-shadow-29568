5ba78b3aa5200e91e52df21f35f0718a0bc70467
Crowdbotics <team@crowdbotics.com>
2021-08-06 15:15:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "ancient_shadow_29568Identifier",
+  url: "https://ancient_shadow_29568Identifier.botics.co",
+  api: "https://ancient_shadow_29568Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
